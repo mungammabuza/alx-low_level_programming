3879883b3f25b7cf02bd661ab70fee80114c8ff5
mungammabuza <munga.m.mabuza@gmail.com>
2023-02-23 14:55:24 -0800

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * 0 through 9
+ *
+ * Return: 1 is c = digit
+ * else: return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
