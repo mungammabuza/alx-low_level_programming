c05a3735c053881ec234207cfdab6ec72310d2d1
mungammabuza <munga.m.mabuza@gmail.com>
2023-03-06 15:11:22 -0800

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack:: string
+ * @needle: substring to be located
+ *
+ * Return: always 0 (uccess)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (haystack = 0; haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = haystack;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
