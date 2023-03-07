dbe08280e7746fb4690fbb8b57642171c7c814b3
mungammabuza <munga.m.mabuza@gmail.com>
2023-03-07 09:29:14 -0800

    8. print diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,7 +8,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagums(int *a, int size);
+void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+int _putchar(char c);
 
 #endif
