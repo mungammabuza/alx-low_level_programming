0e0e87792a4c48ad28a98c1792a0e5bee3b92eeb
mungammabuza <munga.m.mabuza@gmail.com>
2023-03-07 10:06:55 -0800

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,10 +9,10 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; haystack != '\0'; haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
-		char *p = haystack;
+		char *p = needle;
 
 		while (*l == *p && *p != '\0')
 		{
