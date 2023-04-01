22c763bb595ad75d087a914c4d510401852c0adf
mungammabuza <munga.m.mabuza@gmail.com>
2023-03-31 18:47:55 -0700

    Advanced Task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+void print_infrontof_main(void) __attribute__((constructor));
+
+/**
+ * print_infrontof_main - print text before main function
+ *
+ * Return: void
+ */
+void print_infrontof_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back\n");
+}
