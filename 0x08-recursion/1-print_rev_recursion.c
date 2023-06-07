ebd1d2c4d6108ac057ac38168d9429f9f40324f8
mungammabuza <munga.m.mabuza@gmail.com>
2023-06-07 13:53:51 -0700

    Task 1 + 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: string to be reversed
+ * _print_rev_recursion - a function that prints a string in reverse recursively
+ * @s: the string
  */
 void _print_rev_recursion(char *s)
 {
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to be measured
+ * _strlen_recursion - a function returning length of a string
+ * @s: the string
  *
- * Return: length of the string
+ * Return: string length
  */
 int _strlen_recursion(char *s)
 {
-	int longit = 0;
-
+	int strlng = 0;
+	
 	if (*s)
 	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
+		strlng++;
+		strlng = strlng + _strlen_recursion(s + 1);
 	}
-	return (longit);
+	return (strlng);
 }
